46783cd755bed7bf73aa7f076dfc51f916f25ae3
Ganpati RATHIA <gannu.edu@gmail.com>
2025-03-16 06:55:05 +0530

    closed #3 Addde Boiler Plate Project

---
+++ _05_AIPoweredChatBot/src/test/java/com/spring/AIPoweredChatBot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring._01_Spring_Initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
